cc3c38899520f081a560b82144f91a951063d299
Aleksandr Zelenkov <85067573+alksndrz@users.noreply.github.com>
2021-06-17 19:34:49 +0300

    Add files via upload

---
+++ lib/fight_club_colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class FightClubColors {
+
+  FightClubColors._();
+
+  static Color background = Color.fromRGBO(213, 222, 240, 1);
+  static Color greyButton = Color(0x60000000);
+  static Color blueButton = Color(0xFF1C79CE);
+  static Color blackButton = Color(0xDE000000);
+  static Color darkGreyText = Color(0xFF161616);
+  static Color whiteText = Color(0xDDFFFFFF);
+  static Color darkPurple = Color(0xFFC5D1EA);
+
+}

1e23ddb80eb9fd0d0e86e1808c9c65fc53779f66
Aleksandr Zelenkov <85067573+alksndrz@users.noreply.github.com>
2021-09-08 19:34:44 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'pages/main_page.dart';
+
+void main() async {
+  await dotenv.load();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.openSansTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: MainPage(),
+    );
+  }
+}

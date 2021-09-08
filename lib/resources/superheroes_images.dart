3a4dbe1cf6a8f19d3ee661045964f78a334361f5
Aleksandr Zelenkov <85067573+alksndrz@users.noreply.github.com>
2021-09-08 19:33:23 +0300

    Delete lib directory

--- lib/resources/superheroes_images.dart
+++
@@ -1,9 +0,0 @@
-class SuperheroesImages {
-
-  SuperheroesImages._();
-
-  static const ironman = "assets/images/ironman.png";
-  static const hulk = "assets/images/hulk.png";
-  static const superman = "assets/images/superman.png";
-
-}
